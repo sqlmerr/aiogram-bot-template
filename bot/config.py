ddfeaf713bc578a09ab7451da7a3de7b815ce381
sqlmerr <sqlmerr@ya.ru>
2024-03-28 21:39:59 +0300

    🎃 webhook support

--- bot/config.py
+++ bot/config.py
@@ -6,7 +6,15 @@ class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
     MONGO_URL: SecretStr
 
-    model_config = SettingsConfigDict(env_file=".env")
+
+    use_webhooks: bool = True
+    WEB_SERVER_HOST: str = "127.0.0.1"
+    WEB_SERVER_PORT: int = 8080
+    BASE_WEBHOOK_URL: str = "https://9dcc554a0ea38a.lhr.life" #  = "https://example.com"
+    WEBHOOK_SECRET: str = "my-secret"
+    WEBHOOK_PATH: str = "/webhook"
+
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
 
 
 settings = Settings()
